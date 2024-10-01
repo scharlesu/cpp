2ac78186e2e231745713984b2d7b061b448fdde2
scharlesu <scharlesu@gmail.com>
2024-10-01 18:57:21 -0400

    CSS and Random movie or show generator

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const Movie = require("../models/movie");
+const Tvshow = require("../models/tvshow");
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   req.t;
-  res.render("index", { message: req.t("home") });
+  try {
+    const movies = await Movie.find();
+    const tvshows = await Tvshow.find();
+    res.render("index", {
+      message: req.t("home"),
+      movies: movies,
+      tvshows: tvshows,
+    });
+  } catch (error) {
+    console.log(error);
+    res.render("index", { message: error.message });
+  }
 });
 
 module.exports = router;
